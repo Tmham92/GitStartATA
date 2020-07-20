d4776513bc9a382da1154881ff1e2e446f5b0610
Tmham92 <tmham92@gmail.com>
2020-07-20 13:12:46 +0200

    changed way table cells are coloured

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ var tableRow = document.getElementsByTagName("tr");
 var tableData = document.getElementsByTagName("td");
 var tableCell = document.getElementsByClassName("cell");
 
-const playerTurn = document.getElementsByClassName("player-turn");
+const playerTurn = document.querySelector(".player-turn");
 const reset = document.querySelector(".reset");
 
-var counterCol = [0, 0, 0, 0, 0, 0, 0];
-var baseColor = 0;
+//var counterCol = [0, 0, 0, 0, 0, 0, 0];
+//var baseColor = 0;
 
-function changeTableCell(col) {
-    var color;
-    if (baseColor == 0) {
-    color = 'yellow';
-        baseColor = 1;
-    } else {
-    color = 'red';
-        baseColor = 0;
-     }
-     document.getElementById("vakje" + col + "" + counterCol[col]).style.backgroundColor = color;
-    counterCol[col]++;
-}
+//function changeTableCell(col) {
+//    var color;
+//    if (baseColor == 0) {
+//    color = 'yellow';
+//        baseColor = 1;
+//    } else {
+//    color = 'red';
+//        baseColor = 0;
+//     }
+//     document.getElementById("vakje" + col + "" + counterCol[col]).style.backgroundColor = color;
+//    counterCol[col]++;
+//}
 
-function createPlayerName(playerNumber) {
-    var playerName = document.getElementById("playerName" + playerNumber).value;
-    document.getElementById("playerName" + playerNumber + "Filled").innerHTML = playerName;
-}
+//function createPlayerName(playerNumber) {
+//    var playerName = document.getElementById("playerName" + playerNumber).value;
+//    document.getElementById("playerName" + playerNumber + "Filled").innerHTML = playerName;
+//}
 
 
 // Function for selector table cell coordinaties (form 0,0 to 5,6)
@@ -36,6 +36,8 @@ for (let i = 0; i < tableData.length; i++) {
     })
 } 
 
+
+// Creating player names and assign colours
 while (!player1) {
     var player1 = prompt("Player One: ");
 }
@@ -48,4 +50,27 @@ while (!player2) {
 
 player1Color = 'yellow';
 
+var currentPlayer = 1;
+// States player's turn
+playerTurn.textContent = `${player1}'s turn!`;
+
+Array.prototype.forEach.call(tableData, (cell) => {
+    cell.addEventListener('click', changeColor);
+    cell.style.backgroundColor = "white";
+});
+
+function changeColor(e) {
+    let column = e.target.cellIndex;
+    let row = [];
+
+    // start with i = 5 to check for buttom row first
+    for (let i = 5; i >= 0; i--) {
+        if (tableRow[i].children[column].style.backgroundColor == 'white') {
+            row.push(tableRow[i].children[column]);
+            if (currentPlayer === 1) {
+                row[0].style.backgroundColor = player1Color;
+            }
+        }
+    }
+}
 
